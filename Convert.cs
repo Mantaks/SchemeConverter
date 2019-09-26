e470df196664fd96adc65c32b5057591300d9038
Mantaks <50716750+Mantaks@users.noreply.github.com>
2019-09-27 01:46:30 +0300

    Initial commit

---
+++ Convert.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace SchemeConverter
+{
+    public partial class Convert
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void convertButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet currentSheet = Globals.ThisAddIn.GetActiveWorksheet();
+            //  Excel.Workbook workbook = Globals.ThisAddIn.GetActiveWorkbook();
+            Excel.Shapes shapes = currentSheet.Shapes;
+            string a=null;
+            foreach (Excel.Shape item in shapes)
+            {
+                a = item.Name;
+            }
+            Excel.Shape shape = shapes.Item(a);
+            SmartArt smart = shape.SmartArt;
+            SmartArtNodes nodes = smart.AllNodes;
+
+            List<TextFrame2> textFrame2s = new List<TextFrame2>();
+            foreach (SmartArtNode node in nodes)
+            {
+                textFrame2s.Add(node.TextFrame2);
+            }
+            TextRange2 range;
+            List<string> strings = new List<string>();
+            foreach (TextFrame2 textframe2 in textFrame2s)
+            {
+                range = textframe2.TextRange;
+                strings.Add(range.Text);
+            }
+        }
+    }
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+
+namespace SchemeConverter
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+
+        public Excel.Workbook GetActiveWorkbook()
+        {
+            return Application.ActiveWorkbook;
+        }
+        public Excel.Worksheet GetActiveWorksheet()
+        {
+            return (Excel.Worksheet)Application.ActiveSheet;
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
